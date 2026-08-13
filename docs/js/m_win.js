97919474e52ea17355dce85147c4f65e7c66df71
maythecatgirl <cacawki@gmail.com>
2026-08-13 21:55:45 +0930

    Create m_win.js

---
+++ docs/js/m_win.js
@@ -0,0 +1,27 @@
+var _____WB$wombat$assign$function_____=function(name){return (globalThis._wb_wombat && globalThis._wb_wombat.local_init && globalThis._wb_wombat.local_init(name))||globalThis[name];};if(!globalThis.__WB_pmw){globalThis.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opener = _____WB$wombat$assign$function_____("opener");
+<meta http-equiv="refresh" content="0; url=/">
+
+}
+<!--
+     FILE ARCHIVED ON 20:57:22 May 14, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:58:54 Aug 13, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  capture_cache.get: 4.956
+  load_resource: 367.735
+  PetaboxLoader3.resolve: 51.667
+  PetaboxLoader3.datanode: 27.523
+-->
